365cf433451be37a320b3e647caa1ce21b3b6162
Tednn493 <tednn493@gmail.com>
2023-09-06 13:59:55 +0100

    Completed second task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times<0) return "ERROR";
+    let i=0
+    let message=''
+    while (i < times){
+        i++
+        message+=word
+    };  
+    return message;
 };
 
 // Do not edit below this line
