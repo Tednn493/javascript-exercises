958dab5fff8bc67b6431453c23cfb59c843a1862
Tednn493 <tednn493@gmail.com>
2023-09-06 17:25:48 +0100

    fourth task complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrays, ...args) { 
+    for (const target of args){
+        let index=arrays.indexOf(target);
+        if (index!==-1){
+            arrays.splice(index,1)
+        }
+    };
+    return arrays
+    
 };
 
 // Do not edit below this line
