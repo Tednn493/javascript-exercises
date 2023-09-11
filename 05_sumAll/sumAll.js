1365a3f0452650bd8f027e66abfa5348a18c5a9b
Tednn493 <Tednn493@gmail.com>
2023-09-11 13:40:49 +0100

    Fifth task complete however, don't understand why while loop does't work for larger values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    let sum=0
+    if (min<0 || max<0) return 'ERROR';
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    while (min<=max){
+        sum+=min
+        min++
+    };
+    return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
